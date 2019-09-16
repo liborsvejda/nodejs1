f57313d0ddce3638495db544da9883e9fce52b56
Libor Svejda <Libor.Svejda@unicorn.com>
2019-09-16 12:27:56 +0200

    Na adrese /sum vraci ukazku REST sluzby zpracovavajici vstupni parametry z url a vracejici soucet cisel ze vstupu. Pro zpracovani url pouzit modul url a jeho metoda parse.

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 const http = require('http');
+const url = require('url');
 
 let cnt = 0;
 
 http.createServer((req, res) => {
     console.log(`req.url: ${req.url}`);
-    if (req.url == '/') {
+    let parsedUrl = url.parse(req.url, true);
+    if (parsedUrl.pathname == '/') {
         cnt++;
     }
-    if (req.url == '/jsondemo') {
+    if (parsedUrl.pathname == '/jsondemo') {
         res.writeHead(200, {'Content-type': 'application/json'});
         let obj = {};
         obj.action = "test";
         obj.num1 = 123;
         obj.num2 = 456.789;
+        res.end(JSON.stringify(obj));
+    } else if (parsedUrl.pathname == '/sum') {
+        res.writeHead(200, {'Content-type': 'application/json'});
+        let obj = {};
+        obj.action = "sum";
+        obj.num1 = Number(parsedUrl.query["n1"]);
+        obj.num2 = Number(parsedUrl.query["n2"]);
+        obj.sum = obj.num1 + obj.num2;
         res.end(JSON.stringify(obj));
     } else {
         res.writeHead(200, {'Content-type': 'text/html'});
