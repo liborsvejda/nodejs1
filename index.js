16aabe67958e3acf0bd20d0e5cd0bf992e37f3ba
Libor Svejda <Libor.Svejda@unicorn.com>
2019-09-16 09:03:10 +0200

    Http server vraci jednoduchou statickou stranku na adrese http://localhost:8888/.

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-let a = 1;
-console.log(a);
+const http = require('http');
+
+http.createServer((req, res) => {
+    res.writeHead(200, {'Content-type': 'text/html'});
+    res.end('<html lang="cs"><body>Kuk!</body></html>')
+}).listen(8888);
