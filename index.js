396f1cc71724623456e91302e182f06ad4ab2f3f
Libor Svejda <Libor.Svejda@unicorn.com>
2019-09-16 10:00:41 +0200

    Http server vraci pocet volani zakladni adresy.

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const http = require('http');
 let cnt = 0;
 
 http.createServer((req, res) => {
+    console.log(`req.url: ${req.url}`);
+    if (req.url == '/') {
+        cnt++;
+    }
     res.writeHead(200, {'Content-type': 'text/html'});
     res.end(`<html lang="cs"><head><meta charset="UTF-8"></head><body>Počet volání: ${cnt}</body></html>`);
 }).listen(8888);
