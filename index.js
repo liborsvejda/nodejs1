addfef65e34c8c44eec59854d2a2682eb4615f36
Libor Svejda <Libor.Svejda@unicorn.com>
2019-09-16 09:59:45 +0200

    Http server vraci pocet volani, ale na jedno nacteni z prohlizece se zvysi o 2!?

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const http = require('http');
 
+let cnt = 0;
+
 http.createServer((req, res) => {
     res.writeHead(200, {'Content-type': 'text/html'});
-    res.end('<html lang="cs"><body>Kuk!</body></html>')
+    res.end(`<html lang="cs"><head><meta charset="UTF-8"></head><body>Počet volání: ${cnt}</body></html>`);
 }).listen(8888);
